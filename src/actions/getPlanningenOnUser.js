02ac5341d225ef4d735c05de985b709e37b81a8c
karmamocro <o.elhajoui@student.fontys.nl>
2018-06-08 19:24:23 +0200

    added promises

--- src/actions/getPlanningenOnUser.js
+++ src/actions/getPlanningenOnUser.js
@@ -2,36 +2,37 @@ import { GET_PLANNINGENUSER, CHANGE_LOADING_PLANNINENGUSER } from "./types";
 
 import Restful from '../logic/Restful';
 export const getPlanningenOnUser = (data) => dispatch => {
-    dispatch({
-        type: CHANGE_LOADING_PLANNINENGUSER,
-        payload: {
-            loadingPlanningen: true
-        }
-    });
-    Restful.Get(`schedule/onuser/${data.id}`, data.token)
-        .then(response => { return response.json(); })
-        .then(jsonResponse => {
-            if (jsonResponse["success"]) {
+    return new Promise((resolve, reject) => {
+        dispatch({
+            type: CHANGE_LOADING_PLANNINENGUSER,
+            payload: {
+                loadingPlanningen: true
+            }
+        });
+        Restful.Get(`schedule/onuser/${data.id}`, data.token)
+            .then(response => { return response.json(); })
+            .then(jsonResponse => {
+                if (jsonResponse["success"]) {
+                    dispatch({
+                        type: GET_PLANNINGENUSER,
+                        payload: {
+                            planningen: [...jsonResponse["schedule"]],
+                            loadingPlanningen: false
+                        }
+                    });
+                    resolve(jsonResponse["schedule"])
+                }
+
+            })
+            .catch(message => {
+                console.log("catch", message);
                 dispatch({
-                    type: GET_PLANNINGENUSER,
+                    type: CHANGE_LOADING_PLANNINENGUSER,
                     payload: {
-                        planningen: [...jsonResponse["schedule"]],
                         loadingPlanningen: false
                     }
                 });
-            }
-
-        })
-        .catch(message => {
-            console.log("catch", message);
-            dispatch({
-                type: CHANGE_LOADING_PLANNINENGUSER,
-                payload: {
-                    loadingPlanningen: false
-                }
+                reject(message);
             });
-
-        });
-    // 
-
+    });
 };
