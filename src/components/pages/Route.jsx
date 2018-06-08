8173042e737c42c38973c10b3f6d7fdac338bb5a
karmamocro <o.elhajoui@student.fontys.nl>
2018-06-08 19:17:03 +0200

    added initial template for single route from single user

--- src/components/pages/Route.jsx
+++ src/components/pages/Route.jsx
@@ -1,19 +1,119 @@
-import React, { Component, Fragment } from "react";
+import React, { Component, Fragment } from 'react';
 import { connect } from "react-redux";
+import { getUser } from '../../actions/getUser';
+
+import { populatePagination } from '../../actions/populatePagination';
+import { resetPagination } from '../../actions/resetPagination';
+import { getPlanningenOnUser } from '../../actions/getPlanningenOnUser';
+
+import PaginationRow from "./fractions/PaginationRow";
 
 class RoutePage extends Component {
+    constructor(props) {
+        super(props);
+
+        this.userid = props.match.params.idmedewerker;
+    }
+
+    componentWillMount() {
+        this.props.getUser({ id: this.userid, token: this.props.loggedUser.token });
+        this.props.getPlanningenOnUser({ id: this.userid, token: this.props.loggedUser.token });
+    }
 
 
     render() {
+        const planningen = this.props.planningenUser.planningen.map(planning => {
+            return (
+                <tr>
+                    <td>{planning.idplanning}</td>
+                    <td>{planning.datum.split("T")[0]}</td>
+                    <td>{planning.route.routenummer}</td>
+                    <td>{planning.voertuig.voertuigcode}</td>
+                    <td>{planning.echtestarttijd}</td>
+                    <td>{planning.echteeindtijd}</td>
+                    <td>{planning.route.tijdstart}</td>
+                    <td>{planning.route.tijdeind}</td>
+                    <td>{planning.gezien ? "Ja" : "Nee"}</td>
+                </tr>
+            )
+        })
         return (
             <Fragment>
-                route
+                <div className="container-fluid">
+                    <div className="row">
+                        <h1> Informatie over {this.props.userDetail.voornaam} </h1>
+                        <div className="col s12">
+                            <div className="row table-container">
+                                <table className="bordered centered">
+                                    <thead>
+                                        <tr>
+                                            <th>Id</th>
+                                            <th>Gebruikersnaam</th>
+                                            <th>Naam</th>
+                                            <th>Adres</th>
+                                            <th>Stad</th>
+                                            <th>Postcode</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>{this.props.userDetail.id}</td>
+                                            <td>{this.props.userDetail.username}</td>
+                                            <td>{`${this.props.userDetail.voornaam !== "" ? this.props.userDetail.voornaam : "Onbekend"} ${this.props.userDetail.achternaam} `}</td>
+                                            <td>{`${this.props.userDetail.straat} ${this.props.userDetail.huisnummer}`}</td>
+                                            <td>{this.props.userDetail.stad}</td>
+                                            <td>{this.props.userDetail.postcode}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                        <div className="col l12 m12">
+                            <h3 className="center">Planning</h3>
+                            <PaginationRow table="populateRatingTable" />
+                            <div className="col s12">
+                                <div className="row table-container">
+                                    <table className="striped responsive-table">
+                                        <thead>
+                                            <tr>
+                                                <th>Id</th>
+                                                <th>datum</th>
+                                                <th>route</th>
+                                                <th>voertuig</th>
+                                                <th>start tijd</th>
+                                                <th>eind tijd</th>
+                                                <th>route tijd start</th>
+                                                <th>route tijd eind</th>
+                                                <th>gezien</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>{planningen}</tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <a className="waves-effect waves-light btn red">Zet op non-actief</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </Fragment>
         );
     }
 }
 
 const mapStateToProps = state => ({
-    user: state.user
+    user: state.user,
+    pagination: state.pagination,
+    loggedUser: state.user,
+    userDetail: state.user.user,
+    planningenUser: state.planningenUser
 });
-export default connect(mapStateToProps, {})(RoutePage);
+
+const mapDispatchToProps = {
+    populatePagination,
+    resetPagination,
+    getUser,
+    getPlanningenOnUser
+}
+export default connect(mapStateToProps, mapDispatchToProps)(RoutePage);
